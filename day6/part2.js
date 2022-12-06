8c9d3ced5ba9a83f672b5a3f85e2f8fd584e334d
Gustavo Lopez <glopez@emocha.com>
2022-12-06 07:44:37 -0500

    Day 6 challenges

---
+++ day6/part2.js
@@ -0,0 +1,30 @@
+const fs = require("fs");
+const [input] = fs
+  .readFileSync("input.txt", "utf8")
+  .split("\n")
+  .filter((s) => s !== "");
+
+const areAllCharsDifferent = (word) => {
+  const storedChars = [];
+  for (let i = 0; i < word.length; i++) {
+    const char = word[i];
+    if (storedChars.includes(char)) return false;
+    storedChars.push(char);
+  }
+  return true;
+};
+
+let start = 0;
+let end = 14;
+let result = 0;
+for (let index = 0; index < input.length; index++) {
+  const sequence = input.substring(start, end);
+  if (areAllCharsDifferent(sequence)) {
+    result = end;
+    break;
+  }
+  start++;
+  end++;
+}
+
+console.log(result);
