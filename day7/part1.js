2e94f9a33d3803e61eec408cfb09f20968afc40f
Gustavo Lopez <glopez@emocha.com>
2022-12-07 19:06:05 -0500

    Day 7 challenges

---
+++ day7/part1.js
@@ -0,0 +1,57 @@
+const util = require('util')
+const fs = require("fs");
+const [line1, line2, ...input] = fs
+  .readFileSync("input.txt", "utf8")
+  .split("\n")
+  .filter((s) => s !== "");
+
+let currentNavigation = [];
+let tree = { files: [] };
+
+for (let index = 0; index < input.length; index++) {
+  const command = input[index]
+  if (command === '$ ls') {}
+  else if (command.startsWith('$ cd')) {
+    if (command === '$ cd ..') {
+      currentNavigation.pop();
+    } else {
+      const dirNameToCd = command.split('$ cd ')[1];
+      currentNavigation.push(dirNameToCd);
+    }
+  }
+  else if (command.startsWith('dir ')) {
+    const dirName = command.split('dir ')[1];
+    eval(`tree${currentNavigation.map(dir => `['${dir}']`).join('')}['${dirName}'] = { files: [] }`);
+  }
+  else {
+    const [fileSize, fileName] = command.split(' ');
+    eval(`if (!tree${currentNavigation.map(dir => `['${dir}']`).join('')}) tree${currentNavigation.map(dir => `['${dir}']`).join('')}.files = []`);
+    eval(`tree${currentNavigation.map(dir => `['${dir}']`).join('')}.files.push({ fileName: '${fileName}', fileSize: ${fileSize} })`);
+  }
+}
+
+// console.log(util.inspect(tree, { showHidden: false, depth: null, colors: true }));
+
+const dirSizes = {};
+
+const getDirSize = (parentKey, childKey, dir) => {
+  if (!dir) return 0;
+  const filesSize = dir.files.map(f => f.fileSize).reduce((acc, current) => acc + current, 0);
+  const subDirsSize = Object.keys(dir).filter(dir2 => dir2 !== 'files').map(key => {
+    const dirSize = getDirSize(childKey, key, dir[key]);
+    dirSizes[`${childKey}_${key}`] = dirSize;
+    return dirSize;
+  }).reduce((acc, current) => acc + current, 0);
+
+  const total = filesSize + subDirsSize;
+  dirSizes[`${parentKey}_${childKey}`] = total;
+  return total;
+};
+
+Object.keys(tree).filter(key => key !== 'files').map((key) => {
+  return getDirSize('/', key, tree[key]);
+});
+
+const result = Object.values(dirSizes).filter(v => v <= 100000).reduce((acc, current) => acc + current, 0);
+
+console.log(result);
